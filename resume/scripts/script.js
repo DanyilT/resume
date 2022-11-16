39bd6540aecae2396a1192cd874590d64bbdaf66
Danyil Tymchuk <71641218+DanyilT@users.noreply.github.com>
2022-11-16 13:16:30 +0000

    Add files via upload

---
+++ resume/scripts/script.js
@@ -0,0 +1,35 @@
+function about() {
+    var x = document.getElementById("about");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function cs() {
+    var x = document.getElementById("cs");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function math() {
+    var x = document.getElementById("math");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
